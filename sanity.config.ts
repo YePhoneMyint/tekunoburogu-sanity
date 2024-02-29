c0bf10dfc2f1d2bee5a09772f4078cba5b03a98d
Ye Phone Myint <yephonemyint.dev@gmail.com>
2024-03-01 00:08:28 +0630

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Tekuno Buroku',
+
+  projectId: 'pa25ro4s',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
